8aa6b7f854b518d78def27de3f069ce7b28d5bdf
Steven <stevenk@kcsit.pt>
2019-04-24 16:17:13 +0100

    add middleware support

--- src/resolvers.js
+++ src/resolvers.js
@@ -6,6 +6,25 @@ import { withFilter } from 'apollo-server';
 function getModelName(model) {
   return model.options.gqName || upperFirst(model.tableName);
 }
+function defaultMiddleware(f) {
+  return f;
+}
+function setDefaultMiddlewares(sequelize) {
+  Object.keys(sequelize.models).map(key => {
+    const model = sequelize.models[key];
+    if (!model.options.gqMiddleware) model.options.gqMiddleware = {};
+    if (!model.options.gqMiddleware.query)
+      model.options.gqMiddleware.query = defaultMiddleware;
+    if (!model.options.gqMiddleware.create)
+      model.options.gqMiddleware.create = defaultMiddleware;
+    if (!model.options.gqMiddleware.update)
+      model.options.gqMiddleware.update = defaultMiddleware;
+    if (!model.options.gqMiddleware.destroy)
+      model.options.gqMiddleware.destroy = defaultMiddleware;
+    if (!model.options.gqMiddleware.subscribe)
+      model.options.gqMiddleware.subscribe = defaultMiddleware;
+  });
+}
 export function resolvers(
   sequelize,
   pubsub,
@@ -19,6 +38,8 @@ export function resolvers(
     ...getAdditionalresolvers(sequelize, pubsub),
   };
 
+  setDefaultMiddlewares(sequelize);
+
   //associations
   let resolvers = {
     ...Object.keys(sequelize.models).reduce((acc, modelName) => {
@@ -26,33 +47,35 @@ export function resolvers(
       if (!model) return acc;
       modelName = getModelName(model);
       const associations = model.associations || {};
+
       // for each association field call get"AssociationField" method
       const associates = Object.keys(associations).reduce(
         (accAssoc, associationName) => {
           const association = associations[associationName];
-
           const associationFieldName =
             association.as || association.options.name;
           const associationFieldNameType = upperFirst(associationFieldName);
 
-          accAssoc[associationFieldName] = (parent, args, context, info) => {
-            return parent['get' + associationFieldNameType](
-              generateFindArgs(sequelize, args)
-            );
-          };
-
+          accAssoc[
+            associationFieldName
+          ] = association.target.options.gqMiddleware.query(
+            (parent, args, context, info) => {
+              return parent['get' + associationFieldNameType](
+                generateFindArgs(sequelize, args)
+              );
+            }
+          );
           if (association.isMultiAssociation)
-            accAssoc[`_${associationFieldName}Count`] = (
-              parent,
-              args,
-              context,
-              info
-            ) => {
-              return parent['get' + associationFieldNameType]({
-                attributes: [[Sequelize.fn('COUNT', '*'), 'cnt']],
-                ...generateFindArgs(sequelize, args),
-              }).then(result => result[0].get('cnt'));
-            };
+            accAssoc[
+              `_${associationFieldName}Count`
+            ] = association.target.options.gqMiddleware.query(
+              (parent, args, context, info) => {
+                return parent['get' + associationFieldNameType]({
+                  attributes: [[Sequelize.fn('COUNT', '*'), 'cnt']],
+                  ...generateFindArgs(sequelize, args),
+                }).then(result => result[0].get('cnt'));
+              }
+            );
 
           return accAssoc;
         },
@@ -70,23 +93,28 @@ export function resolvers(
       const model = sequelize.models[modelName];
       if (!model) return acc;
       modelName = getModelName(model);
-
       if (model.gqSearch === false) return;
 
       const singular = Sequelize.Utils.singularize(modelName).toLowerCase();
       const plural = Sequelize.Utils.pluralize(modelName).toLowerCase();
 
-      acc[plural] = (parent, args, context, info) => {
-        return model.findAll(generateFindArgs(sequelize, args));
-      };
+      acc[plural] = model.options.gqMiddleware.query(
+        (parent, args, context, info) => {
+          return model.findAll(generateFindArgs(sequelize, args));
+        }
+      );
 
-      acc[`_${plural}Count`] = (parent, args, context, info) => {
-        return model.count(generateFindArgs(sequelize, args));
-      };
+      acc[`_${plural}Count`] = model.options.gqMiddleware.query(
+        (parent, args, context, info) => {
+          return model.count(generateFindArgs(sequelize, args));
+        }
+      );
 
-      acc[singular] = (parent, args, context, info) => {
-        return model.findOne(generateFindArgs(sequelize, args));
-      };
+      acc[singular] = model.options.gqMiddleware.query(
+        (parent, args, context, info) => {
+          return model.findOne(generateFindArgs(sequelize, args));
+        }
+      );
 
       return acc;
     }, {}),
@@ -98,97 +126,71 @@ export function resolvers(
     ...Object.keys(sequelize.models).reduce((acc, modelName) => {
       const model = sequelize.models[modelName];
       if (!model) return acc;
-      modelName = model.options.gqName || upperFirst(model.tableName);
+      modelName = getModelName(model);
 
       const singular = modelName;
       const singularUF = upperFirst(singular);
 
       if (model.gqCreate !== false)
-        acc['create' + singularUF] = async (parent, args, context, info) => {
-          const instance = await model.create(args.input);
-          pubsub &&
-            pubsub.publish('create' + singularUF, {
-              ['create' + singularUF]: instance,
-            });
-          return instance;
-        };
+        acc['create' + singularUF] = model.options.gqMiddleware.create(
+          async (parent, args, context, info) => {
+            const instance = await model.create(args.input);
+            pubsub &&
+              pubsub.publish('create' + singularUF, {
+                ['create' + singularUF]: instance,
+              });
+            return instance;
+          }
+        );
 
       if (model.gqUpdate !== false)
-        acc['update' + singularUF] = async (parent, args, context, info) => {
-          const { input: updateValues } = args;
-          const nwhere = generateFindArgs(sequelize, args);
-          const resultDb = await model.update(updateValues, nwhere);
-          const instances = await model.findAll(nwhere);
-          const result = first(resultDb);
-          if (result && pubsub) {
-            instances.map(instance =>
-              pubsub.publish('update' + singularUF, {
-                ['update' + singularUF]: instance,
-              })
-            );
+        acc['update' + singularUF] = model.options.gqMiddleware.update(
+          async (parent, args, context, info) => {
+            const { input: updateValues } = args;
+            const nwhere = generateFindArgs(sequelize, args);
+            const resultDb = await model.update(updateValues, nwhere);
+            const instances = await model.findAll(nwhere);
+            const result = first(resultDb);
+            if (result && pubsub) {
+              instances.map(instance =>
+                pubsub.publish('update' + singularUF, {
+                  ['update' + singularUF]: instance,
+                })
+              );
+            }
+            return result;
           }
-          return result;
-        };
+        );
 
       if (model.gqDelete !== false)
-        acc['delete' + singularUF] = async (parent, args, context, info) => {
-          const nwhere = generateFindArgs(sequelize, args);
-          const instances = await model.findAll(nwhere);
-          const result = await model.destroy(nwhere);
-          if (result && pubsub) {
-            instances.map(instance =>
-              pubsub.publish('delete' + singularUF, {
-                ['delete' + singularUF]: instance,
-              })
-            );
+        acc['delete' + singularUF] = model.options.gqMiddleware.destroy(
+          async (parent, args, context, info) => {
+            const nwhere = generateFindArgs(sequelize, args);
+            const instances = await model.findAll(nwhere);
+            const result = await model.destroy(nwhere);
+            if (result && pubsub) {
+              instances.map(instance =>
+                pubsub.publish('delete' + singularUF, {
+                  ['delete' + singularUF]: instance,
+                })
+              );
+            }
+            return result;
           }
-          return result;
-        };
+        );
 
       return acc;
     }, {}),
     ...additionalResolvers.mutation,
   };
 
-  const matchOperatorSupport = {
-    eq: (value, match) => value === match,
-    ne: (value, match) => value !== match,
-    gte: (value, match) => value >= match,
-    gt: (value, match) => value > match,
-    lte: (value, match) => value <= match,
-    lt: (value, match) => value < match,
-    in: (value, match) => match.includes(value),
-    notIn: (value, match) => !match.includes(value),
-    like: (value, match) => value.indexOf(match) >= 0,
-    notLike: (value, match) => value.indexOf(match) === -1,
-  };
-
-  function match(container, payload, { where }) {
-    const data = payload[container];
-    if (!data || !where) return true;
-    for (let field in where) {
-      if (!data[field]) continue;
-      for (let operator in matchOperatorSupport) {
-        if (where[field][operator]) {
-          try {
-            return matchOperatorSupport[operator](
-              data[field],
-              where[field][operator]
-            );
-          } catch (error) {
-            continue;
-          }
-        }
-      }
-    }
-    return false;
-  }
   // Subscriptions
   const subscriptions = {
     ...Object.keys(sequelize.models).reduce((acc, modelName) => {
       const model = sequelize.models[modelName];
       if (!model) return acc;
-      modelName = model.options.gqName || upperFirst(model.tableName);
+
+      modelName = getModelName(model);
       const singular = modelName;
       const singularUF = upperFirst(singular);
 
@@ -234,11 +236,44 @@ export function resolvers(
   };
 
   if (Object.keys(subscriptions).length > 0) {
-    console.log('with resolver subscription');
     resolvers.Subscription = subscriptions;
   }
 
   return resolvers;
+}
+
+const matchOperatorSupport = {
+  eq: (value, match) => value === match,
+  ne: (value, match) => value !== match,
+  gte: (value, match) => value >= match,
+  gt: (value, match) => value > match,
+  lte: (value, match) => value <= match,
+  lt: (value, match) => value < match,
+  in: (value, match) => match.includes(value),
+  notIn: (value, match) => !match.includes(value),
+  like: (value, match) => value.indexOf(match) >= 0,
+  notLike: (value, match) => value.indexOf(match) === -1,
+};
+
+function match(container, payload, { where }) {
+  const data = payload[container];
+  if (!data || !where) return true;
+  for (let field in where) {
+    if (!data[field]) continue;
+    for (let operator in matchOperatorSupport) {
+      if (where[field][operator]) {
+        try {
+          return matchOperatorSupport[operator](
+            data[field],
+            where[field][operator]
+          );
+        } catch (error) {
+          continue;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 function generateFindArgs(sequelize, args) {
